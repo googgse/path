a2de3b1fd42bd41a774e0330554ab768790c33f8
impossible98 <ago2chris@outlook.com>
2022-11-02 13:56:49 +0800

    feat: add Join

--- path.go
+++ path.go
@@ -6,6 +6,7 @@ import (
 	"path"
 )
 
+// 判断所给路径文件/文件夹是否存在
 func Exists(path string) bool {
 	_, err := os.Stat(path)
 	// control flows
@@ -17,11 +18,13 @@ func Exists(path string) bool {
 	return true
 }
 
+// 判断所给路径是否为绝对路径
 func IsAbs(_path string) bool {
 	// return value
 	return path.IsAbs(_path)
 }
 
+// 判断所给路径是否为文件夹
 func IsDir(path string) bool {
 	stat, err := os.Stat(path)
 	// control flows
@@ -33,7 +36,13 @@ func IsDir(path string) bool {
 	return stat.IsDir()
 }
 
+// 判断所给路径是否为文件
 func IsFile(path string) bool {
 	// return value
 	return !IsDir(path)
+}
+
+// 将任意数量的路径元素放入一个单一路径
+func Join(_path ...string) string {
+	return path.Join(_path...)
 }
