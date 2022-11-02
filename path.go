11176bcc1f9a7191f3acf6cbdd25295fac9a3c42
impossible98 <ago2chris@outlook.com>
2022-11-02 13:36:17 +0800

    refactor: refactor code

--- path.go
+++ path.go
@@ -1,30 +1,39 @@
 package path
 
 import (
-	// import local packages
-	"github.com/googgse/path/lib"
+	// import built-in packages
+	"os"
+	"path"
 )
 
-// 判断所给路径文件/文件夹是否存在
 func Exists(path string) bool {
+	_, err := os.Stat(path)
+	// control flows
+	if err != nil {
+		// return value
+		return os.IsExist(err)
+	}
 	// return value
-	return lib.Exists(path)
+	return true
 }
 
-// 判断所给路径是否为绝对路径
-func IsAbs(path string) bool {
+func IsAbs(_path string) bool {
 	// return value
-	return lib.IsAbs(path)
+	return path.IsAbs(_path)
 }
 
-// 判断所给路径是否为文件夹
 func IsDir(path string) bool {
+	stat, err := os.Stat(path)
+	// control flows
+	if err != nil {
+		// return value
+		return false
+	}
 	// return value
-	return lib.IsDir(path)
+	return stat.IsDir()
 }
 
-// 判断所给路径是否为文件
 func IsFile(path string) bool {
 	// return value
-	return lib.IsFile(path)
+	return !IsDir(path)
 }
